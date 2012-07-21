88050892fbea157ee9c35cbe502aac1e14729ce0
Raphfrk <raphfrk@gmail.com>
2012-07-21 09:24:19 +0100

    Initial commit

---
+++ src/main/java/com/raphfrk/ballotimage/gui/GUIMain.java
@@ -0,0 +1,13 @@
+package com.raphfrk.ballotimage.gui;
+
+/**
+ * Hello world!
+ *
+ */
+public class GUIMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/raphfrk/ballotimage/gui/GUIMainTest.java
@@ -0,0 +1,14 @@
+package org.spout.engine.world;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GUIMainTest {
+
+	@Test
+	public void test() {
+		// Placeholder
+	}
+
+}
